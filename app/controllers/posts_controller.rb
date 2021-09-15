ee39c65c513d5b34b10510245bf63c9a16f9dd7c
Dani <dani8439@aol.com>
2021-09-15 13:05:27 -0400

    setting up ActiveStorage to store an image.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
 
     def index 
     end 
+
+    private 
+    
+    def post_params 
+        params.require(:post).permit(:description, :image)
+    end 
 end
