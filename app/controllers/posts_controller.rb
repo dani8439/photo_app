6af569da0380c4fc7635acd3e51ba40a907c2a98
Dani <dani8439@aol.com>
2021-09-15 13:20:19 -0400

    building out form to create post, updated controller, can attach image.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,16 @@ class PostsController < ApplicationController
     def index 
     end 
 
+    def new 
+        @post = Post.new
+    end
+
+    def create 
+        @post = Post.create(post_params)
+
+        redirect_to posts_path
+    end
+
     private 
     
     def post_params 
