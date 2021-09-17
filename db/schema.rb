93f1be91eb263815b8d055a467b60983fe93c62a
Dani <dani8439@aol.com>
2021-09-17 15:10:54 -0400

    added UserName to signup page, schema, created RegistrationsController and aliased under routes,  User can now be created through creation page.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_184418) do
+ActiveRecord::Schema.define(version: 2021_09_17_185207) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,8 +47,10 @@ ActiveRecord::Schema.define(version: 2021_09_17_184418) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
