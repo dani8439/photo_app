2f7af9f9320d7c91950ac116921792da623d164e
Dani <dani8439@aol.com>
2021-09-17 16:17:28 -0400

    only showing users posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,8 @@
 class Post < ApplicationRecord
+    belongs_to :user
     validates :user_id, presence: true 
     validates :image, presence: true 
     has_one_attached :image
-
-    belongs_to :user
     # has_attached_file :image, styles: { :medium => "640px" }
     # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
