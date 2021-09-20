db89984b9f9cb618fa4aa212b89c22d5f95c8b7a
Dani <dani8439@aol.com>
2021-09-20 16:17:23 -0400

    error in rspec, devise methods not loading properly? Have to research.

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -2,13 +2,24 @@
 
 # feature 'Creating posts' do 
 #     background do 
-#         user = create :user 
-#         sign_in user
-    
+#         create :user,  email: "me@mail.com", user_name: "SomeGuy", password: "123ABC", id: 4
+#         new_user_session_path 'me@mail.com', "123ABC"
+#     end 
+
+#     scenario 'signs the user in successfully with a valid email and password' do 
+#         new_user_session_path 'me@mail.com', "123ABC"
+#     end 
+
+#     def sign_in_with(email, password)
+#         visit new_user_session_path 
+#         fill_in 'Email', with: email 
+#         fill_in 'Password', with: password 
+#         click_button 'Sign In'
 #     end 
 
 #     scenario 'can create a new post' do 
 #         visit '/'
+#         # sign in is failing, cannot see new post link
 #         click_link 'New Post'
 #         attach_file('Image', "spec/files/images/puppy.jpg")
 #         fill_in 'Description', with: "Puppy"
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -22,20 +22,4 @@ RSpec.describe Post, :type => :model do
     it "is valid with a description, and image" do 
         expect(post).to be_valid
     end 
-
-    # it "is valid with a username, password, and email" do 
-    #     expect(user).to be_valid
-    # end 
-
-    # it "is not valid without a password" do 
-    #     expect(User.new(user_name: "Name")).not_to be_valid
-    # end 
-
-    # it "is not valid without a email" do 
-    #     expect(User.new(password: "Password")).not_to be_valid
-    # end 
-
-    # it "is not valid without a username" do 
-    #     expect(User.new(email: "Boo@mail.com")).not_to be_valid
-    # end 
 end 
